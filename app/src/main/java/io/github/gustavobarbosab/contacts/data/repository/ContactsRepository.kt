8f4b694f423c2d84169f5b47b6074b5bbc7d536b
gustavobarbosab <gustavobarbosabarreto@gmail.com>
2019-09-26 23:19:04 -0300

    Adicionando todo para alterar o model para a camada de view

--- app/src/main/java/io/github/gustavobarbosab/contacts/data/repository/ContactsRepository.kt
+++ app/src/main/java/io/github/gustavobarbosab/contacts/data/repository/ContactsRepository.kt
@@ -4,5 +4,6 @@ import io.github.gustavobarbosab.contacts.domain.ContactDto
 import io.github.gustavobarbosab.contacts.utils.Result
 
 interface ContactsRepository {
+    // TODO fazer o retorno de outro model, deixando o ContactDto fora da camada de view
     suspend fun getContacts(force: Boolean): Result<List<ContactDto>>
 }
